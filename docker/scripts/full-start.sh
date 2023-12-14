fc013fd0bff5c8c34149d496e74f92d904138832
patrik200 <patrik@DESKTOP-2KP97KL>
2023-12-14 09:34:10 +0300

    sdgf

--- docker/scripts/full-start.sh
+++
@@ -1,1 +0,0 @@
-./cnly.sh -n 'backend,frontend,landing' 'cd packages/back/backend && npm run start:prod' 'cd packages/front/frontend && npm run start:prod' 'cd packages/front/landing && npm run start:prod'
